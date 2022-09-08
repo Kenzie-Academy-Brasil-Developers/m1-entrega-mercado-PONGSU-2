952946932395ae38611bb165c39706358b43964b
Gabriel Carvalho Maciel <94143451+PONGSU@users.noreply.github.com>
2022-09-08 06:47:30 -0300

    Update products.js
    Adaptando o Código pra ficar de acordo com a rubrica.
    O meu anterior pegava um html, só com Nav E Main e criava tudo :( . 
    Achei bem melhor e eficiente o meu, mas fiz esse pra ficar igual ao que era pedido

--- products.js
+++ products.js
@@ -11,21 +11,21 @@ const products = [
     price: 5.69,
     category: "Frutas",
     image: "./img/products/product_2.svg",
-    imageDescription: "",
+    imageDescription: "Foto de varios cachos de bananas maduras empilhados",
   },
   {
     title: "Mamão",
     price: 4.99,
     category: "Frutas",
     image: "./img/products/product_3.svg",
-    imageDescription: "",
+    imageDescription: "Uma foto com 9 mamões juntos e focados, a coloração predominante deles é verde com algumas regiões amareladas",
   },
   {
     title: "Maçã",
     price: 9.2,
     category: "Frutas",
     image: "./img/products/product_4.svg",
-    imageDescription: "",
+    imageDescription: "Foto de uma maça verde, existe um reflexo da mesma em seu inferior, e ao fundo uma imagem desfocada que lembra galhos e folhas de arvore vistos de baixo contra a luz solar",
   },
   {
     title: "Refrigerante",
@@ -39,7 +39,7 @@ const products = [
     price: 8.99,
     category: "Bebidas",
     image: "./img/products/product_6.svg",
-    imageDescription: "",
+    imageDescription: "Na foto Uma garrava de vinho Monte Sasso-Primitivo di Manduria, de rótudo amarronzado, sobre uma mesa de madeira. Ao lado direito sobre a mesa uma especie de churrasqueira portatil com chamas altas. Do lado esquerdo sobre a mesa existe um pano branco com rosa. Ao fundo bastante desfocado uma vegetacão onde predomina a grama, mas é possivel perceber alguns arbustos e a parte inferior do tronco de uma arvore",
   },
   {
     title: "Água Tônica",
@@ -53,7 +53,7 @@ const products = [
     price: 8.99,
     category: "Bebidas",
     image: "./img/products/product_8.svg",
-    imageDescription: "",
+    imageDescription: "Uma foto tirada de cima mostrando uma pilha de cocos verdes todos juntos",
   },
 
   {
@@ -61,7 +61,7 @@ const products = [
     price: 8.99,
     category: "Higiene",
     image: "./img/products/product_9.svg",
-    imageDescription: "",
+    imageDescription: "Foto de dois sabonetes em formato de paralelepipedo, um rosa a esquerda posicionado na vertical com uma leve inclinação e um verde, envolvido por um laço tambem verde e posicionado na horizontal, também com leve inclinação. Ao fundo uma superficie clara e lisa",
   },
 
   {
@@ -69,7 +69,7 @@ const products = [
     price: 8.99,
     category: "Higiene",
     image: "./img/products/product_10.svg",
-    imageDescription: "",
+    imageDescription: "Um recipiente grande de detergente 'Botanical Origin' de cor vermelha com a tampa branca, posicionado a direita da foto. A esquerda uma peça de madeira branca, com a parte superior ondulada, nessa peça está apoiado um talher que em sua ponta se ve parte de um macarrão enrolado. O fundo é liso de cor lilas",
   },
 
   {
@@ -77,7 +77,7 @@ const products = [
     price: 8.99,
     category: "Higiene",
     image: "./img/products/product_11.svg",
-    imageDescription: "",
+    imageDescription: "Na foto aparece apenas uma mão e antebraço vindo da parte superior esquerda, na mão uma luva cirurgica azul claro, segurando um borrifador também azul que por sua vez contem um líquido transparente. O borrififador está apontado para o chão de um comodo feito todo de madeira.",
   },
 
   {
@@ -93,15 +93,14 @@ function fixProductsImages() {
   for (let i = 0; i < products.length; i++) {
     if (products[i].image == undefined) {
       products[i].image = './img/products/no-img.svg'
-      products[i].imageDescription = 'imagem ausente'
-    } else {
-      products[i].imageDescription = `Foto de um(a) ${products[i].title}`
+      products[i].imageDescription = "Produto sem imagem"
     }
   }
 }
 fixProductsImages()
 
 let main = document.getElementsByClassName('container')[0]
+
 let categorys = []
 
 function getCategorys() {
@@ -114,28 +113,20 @@ function getCategorys() {
 }
 getCategorys()
 
-function createSections() {
+let section_fruits = document.getElementsByClassName('products-content fruits')[0]
+let section_drinks = document.getElementsByClassName('products-content drinks')[0]
+let section_hygiene = document.getElementsByClassName('products-content hygiene')[0]
+
+function fillSections() {
   for (let i = 0; i < categorys.length; i++) {
-    let product_section = document.createElement('section')
-    product_section.classList.add('products-section')
-
-    let section_tittle = document.createElement('h1')
-    section_tittle.innerText = categorys[i]
-    product_section.appendChild(section_tittle)
-
-    let section_products = document.createElement('div')
-    section_products.classList.add(`products-content`)
-    section_products.id = categorys[i]
-    product_section.appendChild(section_products)
 
     let products_list = document.createElement('ul')
-    section_products.appendChild(products_list)
 
     for (let idx = 0; idx < products.length; idx++) {
       if (products[idx].category == categorys[i]) {
         let product = document.createElement('li')
         product.classList.add('product')
-        product.innerHTML += `<img src="${products[idx].image}" alt="${products[idx].imageDescription}" title="" class="product-img"/>`
+        product.innerHTML += `<img src="${products[idx].image}" alt="${products[idx].imageDescription}" title="${products[idx].imageDescription}" class="product-img"/>`
         let product_main = document.createElement('div')
         product_main.classList.add('product-main')
 
@@ -158,9 +149,15 @@ function createSections() {
         products_list.appendChild(product)
       }
     }
+    if (categorys[i] == 'Frutas') {
+      section_fruits.appendChild(products_list)
+    } else if (categorys[i] == 'Bebidas') {
+      section_drinks.appendChild(products_list)
+    } else if (categorys[i] == 'Higiene') {
+      section_hygiene.appendChild(products_list)
+    }
 
-    main.appendChild(product_section)
   }
 }
 
-createSections()
+fillSections()
